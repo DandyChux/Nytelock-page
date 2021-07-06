18bada85f1d60ff94e6636166da58926b895dcb1
Chukwuma Okoroji <dandychux@gmail.com>
2021-07-05 22:47:11 -0400

    Update
    Updates:
    - styled the description containers
    - fixed script to display div on click
    
    What To Do:
    - add script for changing active class

--- main.js
+++ main.js
@@ -1,20 +1,11 @@
-//  Get the container element 
-var boxContainer = document.getElementsByClassName('box-container');
+jQuery(document).ready(function($) {
+    $('.description') .hide()
+$('a[href^="#"]').on('click', function(event) {
+    $('.description') .hide()
+    
+    var target = $(this).attr('href');
 
-//  Get all the links inside the boxContainer 
-var links = boxContainer.getElementsByTagName('a');
+    $('.description'+target).toggle();
+})
 
-//  Loop through and add the active class to the current link 
-for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener('click', function() {
-        var current = document.getElementsByClassName('active');
-
-        //  If there is no active class
-        // if (current.length > 0) {
-        //     current[0].className = current[0].className.replace('active', '');
-        // }
-
-        //  Add the active class to the current link 
-        this.className += 'active';
-    });
-}
+})
